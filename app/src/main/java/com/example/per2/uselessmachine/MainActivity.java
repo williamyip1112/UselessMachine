29cab44ef654b5a2f513c76fc76f50be3a6e9b10
William <williamyip1112@gmail.com>
2018-08-29 08:50:26 -0700

    Added CountDownTimer to switch to turn off after 5 seconds Switching off early cancels the timer

--- app/src/main/java/com/example/per2/uselessmachine/MainActivity.java
+++ app/src/main/java/com/example/per2/uselessmachine/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.per2.uselessmachine;
 
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.Switch;
@@ -10,7 +12,7 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
     private Button buttonsSelfDestruct;
     private Switch switchUseless;
-
+    public static final String TAG = MainActivity.class.getSimpleName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,6 +29,7 @@ public class MainActivity extends AppCompatActivity {
             {
               if(checked)
               {
+                  startSwitchOffTimer();
                   Toast.makeText(MainActivity.this, "On", Toast.LENGTH_SHORT).show();
               }
               else
@@ -35,6 +38,27 @@ public class MainActivity extends AppCompatActivity {
               }
             }
         });
+    }
+
+    private void startSwitchOffTimer()
+    {
+        new CountDownTimer(5000, 5000) {
+            @Override
+            public void onTick(long millisUntilFinished)
+            {
+                if(!switchUseless.isChecked())
+                {
+                   Log.d(TAG, "OnTick: canceling");
+                }
+            }
+
+            @Override
+            public void onFinish()
+            {
+                switchUseless.setChecked(false);
+                Log.d(TAG, "onFinished: switch set to false");
+            }
+        }.start();
     }
 
     private void wireWidgets()
